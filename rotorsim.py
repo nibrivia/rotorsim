fbde44c93dc904d168a8da414b44507615a7c3c0
Olivia Brode-Roger <nibr@mit.edu>
2019-10-23 16:27:07 -0400

    Support multiple rotor switches

--- rotorsim.py
+++ rotorsim.py
@@ -1,9 +1,12 @@
 import random
+import math
 
-N_TOR   = 4
-N_ROTOR = 1
+N_TOR   = 5
+N_ROTOR = 2
+N_MATCHINGS = N_TOR - 1 #don't link back to yourself
+MATCHINGS_PER_ROTOR = math.ceil(N_MATCHINGS / N_ROTOR)
 
-random.seed(375620)
+#random.seed(375620)
 
 def generate_matchings():
     all_matchings = []
@@ -15,9 +18,11 @@ def generate_matchings():
 
     return all_matchings
 
-def generate_demand():
+def generate_demand(min_demand = 0, max_demand = 1):
+    assert 0 <= min_demand
+    assert min_demand <= max_demand
     # Demand is scaled: 1 how much can be sent in 1 matching slot
-    return [[random.random() for dst in range(N_TOR)] for src in range(N_TOR)]
+    return [[random.uniform(min_demand, max_demand) for dst in range(N_TOR)] for src in range(N_TOR)]
 
 def add_demand(old, new):
     for row, _ in enumerate(old):
@@ -32,6 +37,7 @@ def print_demand(demand, prefix = ""):
 
 if __name__ == "__main__":
     print("Starting simulation with %d ToR switches and %d rotor switches" % (N_TOR, N_ROTOR))
+    print("There are %d matchings, with %d per rotor switch" % (N_MATCHINGS, MATCHINGS_PER_ROTOR))
 
     matchings_by_slot = generate_matchings()
 
@@ -46,31 +52,39 @@ if __name__ == "__main__":
     # Initial demand
     demand = generate_demand()
 
+    # Rotor matchings
+    rotor_matches = [r for r in range(N_ROTOR)]
+
     print()
     N_CYCLES = 10
     for cycle in range(N_CYCLES):
         print("Cycle %d" % cycle)
 
         # Send data
-        for slot, matchings in enumerate(matchings_by_slot):
+        for slot in range(MATCHINGS_PER_ROTOR):
             print("    Slot %d" % slot)
             print_demand(demand, prefix = "     ")
 
-            # Go through every matching and send as much as we can
-            for src, dst in enumerate(matchings):
-                # Send to remote destination
-                data_sent = min(max(0, demand[src][dst]), 1)
-                print("        %d->%d: sending %.2f" % (src, dst, data_sent))
-                demand[src][dst] -= data_sent
+            # Go through each Rotor switch
+            for rotor_n in range(N_ROTOR):
+                print("      Rotor %d" % rotor_n)
+                rotor_matchings = matchings_by_slot[slot*MATCHINGS_PER_ROTOR+rotor_n]
 
-                # Send locally
-                data_sent = min(max(0, demand[src][src]), 1)
-                print("        %d->%d: sending %.2f" % (src, src, data_sent))
-                demand[src][src] -= data_sent
+                # Go through every matching and send as much as we can
+                for src, dst in enumerate(rotor_matchings):
+                    # Send to remote destination
+                    data_sent = min(max(0, demand[src][dst]), 1)
+                    print("        %d->%d: sending %.2f" % (src, dst, data_sent))
+                    demand[src][dst] -= data_sent
+
+                    # Send locally
+                    data_sent = min(max(0, demand[src][src]), 1)
+                    #print("        %d->%d: sending %.2f" % (src, src, data_sent))
+                    demand[src][src] -= data_sent
 
 
-
-        # Generate demand on every cycle
-        demand = add_demand(demand, generate_demand())
+            # Generate demand on every slot
+            print()
+            demand = add_demand(demand, generate_demand(max_demand = 1.5*1/(N_TOR-1)))
 
 
