1c1c6199c3c12892d1c5a2c0b0a0906393619dc4
Olivia Brode-Roger <nibr@mit.edu>
2019-11-10 20:31:18 -0500

    refactoring out buffer

---
+++ buffer.py
@@ -0,0 +1,67 @@
+import collections 
+from logger import Log
+
+LOG = Log()
+DO_LOG = False
+VERBOSE = False
+
+class Buffer():
+    def __init__(self, name = ""):
+        self.packets = collections.deque()
+        self.name = str(name)
+        self.owner, self.q_name = str(name).split(".")
+        self.count = 0
+
+        # Cached, this is used a lot
+        self.size = 0
+
+    def send(self, to, num_packets):
+        assert len(self.packets) >= num_packets, "Sending more packets than in queue %s" % self
+
+        moving_packets = [self.packets.popleft() for _ in range(num_packets)]
+        to.recv(moving_packets)
+
+        self.size = len(self.packets)
+        log(self, to, moving_packets)
+
+        if VERBOSE and num_packets > 0:
+            print("        \033[01m%s -> %s: %2d\033[00m"
+                    % (self, to, num_packets))
+
+    def recv(self, packets):
+        self.packets.extend(packets)
+        self.size = len(self.packets)
+
+    def add(self, val):
+        new_packets = [self.count+i for i in range(val)]
+        self.count += val
+        self.packets.extend(new_packets)
+        self.size = len(self.packets)
+
+        log("demand", self, new_packets)
+
+
+    def __str__(self):
+        return "%s" % self.name
+
+class EndPoint:
+    def __init__(self):
+        self.to_send = []
+        self.received = []
+
+# TODO use logger process
+def log(src, dst, packets):
+    if not DO_LOG:
+        return
+
+    t = T.T
+    if isinstance(src, str):
+        for p in packets:
+            LOG.log("%.3f, %s, 0, %s, %s, %d\n" %
+                    (t, src, dst.owner, dst.q_name, p))
+            t += 1/(PACKETS_PER_SLOT*N_SLOTS)
+    else:
+        for p in packets:
+            LOG.log("%.3f, %s, %s, %s, %s, %d\n" %
+                    (t, src.owner, src.q_name, dst.owner, dst.q_name, p))
+            t += 1/(PACKETS_PER_SLOT*2)
--- switches.py
+++ switches.py
@@ -1,8 +1,8 @@
 import random
 import math
 import sys
-import collections
 from logger import *
+from buffer import *
 
 N_TOR   = 17
 N_ROTOR = 4
@@ -12,26 +12,8 @@ N_CYCLES = 5
 
 VERBOSE = False
 
-LOG = Log()
 
-DO_LOG = False
 
-# TODO use logger process
-def log(src, dst, packets):
-    if not DO_LOG:
-        return
-
-    t = T.T
-    if isinstance(src, str):
-        for p in packets:
-            LOG.log("%.3f, %s, 0, %s, %s, %d\n" %
-                    (t, src, dst.owner, dst.q_name, p))
-            t += 1/(PACKETS_PER_SLOT*N_SLOTS)
-    else:
-        for p in packets:
-            LOG.log("%.3f, %s, %s, %s, %s, %d\n" %
-                    (t, src.owner, src.q_name, dst.owner, dst.q_name, p))
-            t += 1/(PACKETS_PER_SLOT*2)
 
 def close_log():
     LOG.close_log()
@@ -54,50 +36,6 @@ global T
 T = Time()
 
 PACKETS_PER_SLOT = 100
-class Buffer():
-    def __init__(self, name = ""):
-        self.packets = collections.deque()
-        self.name = str(name)
-        self.owner, self.q_name = str(name).split(".")
-        self.count = 0
-
-        # Cached, this is used a lot
-        self.size = 0
-
-    def send(self, to, num_packets):
-        assert len(self.packets) >= num_packets, "Sending more packets than in queue %s" % self
-
-        moving_packets = [self.packets.popleft() for _ in range(num_packets)]
-        to.recv(moving_packets)
-
-        self.size = len(self.packets)
-        log(self, to, moving_packets)
-
-        if VERBOSE and num_packets > 0:
-            print("        \033[01m%s -> %s: %2d\033[00m"
-                    % (self, to, num_packets))
-
-    def recv(self, packets):
-        self.packets.extend(packets)
-        self.size = len(self.packets)
-
-    def add(self, val):
-        new_packets = [self.count+i for i in range(val)]
-        self.count += val
-        self.packets.extend(new_packets)
-        self.size = len(self.packets)
-
-        log("demand", self, new_packets)
-
-
-    def __str__(self):
-        return "%s" % self.name
-
-class EndPoint:
-    def __init__(self):
-        self.to_send = []
-        self.received = []
-
 class ToRSwitch:
     def __init__(self, name = "", n_tor = 0):
         # Index by who to send to
