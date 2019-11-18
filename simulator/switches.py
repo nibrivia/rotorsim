9788e411e779596efdd2c723db402acd010ad423
Olivia Brode-Roger <nibr@mit.edu>
2019-11-18 17:22:18 -0500

    Reduced complexity from n_tor^3 to n_tor^2 ish

--- simulator/switches.py
+++ simulator/switches.py
@@ -1,17 +1,22 @@
 import sys
 from buffer import *
 from helpers import *
-from event import delay
+from event import delay, R
+from functools import lru_cache
 
 
 class ToRSwitch:
-    def __init__(self, name, n_tor, n_rotor, packets_per_slot, logger, verbose):
+    def __init__(self, name, n_tor, n_rotor, packets_per_slot, logger, verbose, timer):
         # Index by who to send to
-        self.id = int(name)
-        self.n_tor = n_tor
+        self.id      = int(name)
+        self.n_tor   = n_tor
         self.n_rotor = n_rotor
-        self.packets_per_slot = packets_per_slot
         self.verbose = verbose
+        self.timer   = timer
+        self.packets_per_slot = packets_per_slot
+
+        # Demand
+        self.tot_demand = 0
 
         self.buffers = { (src, dst) : Buffer(
             name = "%s.%s->%s" % (self.id, src, dst),
@@ -37,8 +42,12 @@ class ToRSwitch:
         if dst.id != self.id:
             self.buffers[(self.id, dst.id)].add_n(amount)
             self.capacity[dst.id] -= amount
+            self.tot_demand += amount
 
     def send(self, rotor_id, flow, amount):
+        if amount == 0:
+            return
+
         dst, link_remaining = self.connections[rotor_id]
 
         # Check link capacity
@@ -58,11 +67,14 @@ class ToRSwitch:
         dst.recv(flow_dst, amount)
         self.capacity[flow_dst] += amount
 
+        self.tot_demand -= amount
+
         # Return remaining link capacity
         return link_remaining
 
     def recv(self, dst_id, amount):
         if dst_id != self.id:
+            self.tot_demand += amount
             self.capacity[dst_id] -= amount
 
     def connect_to(self, rotor_id, tor):
@@ -77,13 +89,15 @@ class ToRSwitch:
 
     def indirect_traffic_to(self, dst):
         # Returns indirect traffic to dst
-        return {(s, d) : b for (s, d), b in self.buffers.items()
-                    if d == dst.id and s != self.id and b.size > 0}
+        flows = [(s, dst.id) for s in range(self.n_tor) if s != self.id]
+        return [(f, self.buffers[f]) for f in flows
+                    if self.buffers[f].size > 0]
 
     def direct_traffic(self, dst):
         # Returns direct traffic except to dst
-        return {(s, d) : b for (s, d), b in self.buffers.items()
-                    if s == self.id and d != dst.id and b.size > 0}
+        flows = [(self.id, d) for d in range(self.n_tor) if d != dst.id]
+        return [(f, self.buffers[f]) for f in flows
+                    if self.buffers[f].size > 0]
 
     @delay(.001)
     def send_old_indirect(self, rotor_id):
@@ -96,15 +110,16 @@ class ToRSwitch:
         buffers = self.indirect_traffic_to(dst)
 
         # Verify link violations
-        total_send = sum(b.size for b in buffers.values())
-        assert total_send <= self.n_rotor*self.packets_per_slot, \
-                "%s->%s old indirect %d > capacity" % (self, dst, total_send)
+        if False:
+            total_send = sum(b.size for _, b in buffers)
+            assert total_send <= self.n_rotor*self.packets_per_slot, \
+                    "%s->%s old indirect %d > capacity" % (self, dst, total_send)
 
         # Send the data
         sent = 1
         while sent > 0:
             sent = 0
-            for flow, b in buffers.items():
+            for flow, b in buffers:
                 if b.size == 0:
                     continue
                 if self.connections[rotor_id][1] == 0:
@@ -117,15 +132,15 @@ class ToRSwitch:
     @delay(.002)
     def send_direct(self, rotor_id):
         self.vprint("Direct: %s:%d" % (self, rotor_id), 2)
+
         # Get connection data
         dst, remaining = self.connections[rotor_id]
-
-        #print("         %s to %s via %s remaining %s" % (self, dst, rotor_id, remaining))
         flow = (self.id, dst.id)
-        n_sending = bound(0, self.buffers[flow].size, remaining)
+        amount = bound(0, self.buffers[flow].size, remaining)
+
         self.send(rotor_id = rotor_id,
                   flow = flow,
-                  amount = n_sending)
+                  amount = amount)
 
     @delay(delay_t = .003)
     def send_new_indirect(self, rotor_id):
@@ -141,14 +156,14 @@ class ToRSwitch:
         # TODO offer/accept protocol
 
         # This iterates to balance the remaining capacity equally across flows
-        amounts = {flow:0 for flow in traffic}
+        amounts = {flow:0 for flow, _ in traffic}
         #print(amounts)
         change = 1
         while change > 0:
             change = 0
 
             # TODO, not just +1, do it the (faster) divide way
-            for flow, b in traffic.items():
+            for flow, b in traffic:
                 flow_src, flow_dst = flow
                 current = amounts[flow]
                 #assert capacities[flow_dst] <= self.packets_per_slot
