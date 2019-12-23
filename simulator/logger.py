1c26eb0677dfeeb7cdb6a231cc045b0aee4870dc
Olivia Brode-Roger <nibr@mit.edu>
2019-12-23 18:50:47 -0500

    removed redundant info from .csv
    Former-commit-id: def1a50e48fe9062a8b50575fc5d6926324996fa

--- simulator/logger.py
+++ simulator/logger.py
@@ -5,19 +5,17 @@ class Log:
         self.cache = [] # Use array to avoid n^2 string append
 
         # Initialize the .csv
-        print("time, src, dst, flow, flow_src, flow_dst, packet",
+        print("time, src, dst, flow, packet",
                 file = self.file)
 
     def add_timer(self, timer):
         self.timer = timer
 
     def log(self, src, dst, packet):
-        msg = "%.3f,%s,%s,%s,%s,%s,%s\n" % \
+        msg = "%.3f,%s,%s,%s,%s\n" % \
                         (self.timer.time,
                             src.id, dst.id,
                             packet.flow.flow_id,
-                            packet.src.id,
-                            packet.dst.id,
                             packet.seq_num)
         self.cache.append(msg)
         if len(self.cache) > 100:
