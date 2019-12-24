8530ff87e9425716430a65caaffc9a121a6881fb
Olivia Brode-Roger <nibr@mit.edu>
2019-12-24 15:04:05 -0500

    average sizes added to flows, still needs refactoring
    Former-commit-id: 8ac9709815b6e97fc315a24bee52b4616cc1651d

--- simulator/workloads/chen.py
+++ simulator/workloads/chen.py
@@ -9,33 +9,39 @@ LARGE_FLOW_PROB  = 0.25
 assert LARGE_FLOW_PROB + SMALL_FLOW_PROB + MEDIUM_FLOW_PROB == 1
 
 def chen_distribution(
-	size_of_small_flows =1,    # MB 
-	size_of_medium_flows=10,   # MB 
-	size_of_large_flows =1000, # MB
+    size_of_small_flows =1,    # MB 
+    size_of_medium_flows=10,   # MB 
+    size_of_large_flows =1000, # MB
 ):
 
-	# obtain random number from [0, 1]
-	random_float = random.random()
+    # obtain random number from [0, 1]
+    random_float = random.random()
 
-	# case: large flow
-	if random_float <= LARGE_FLOW_PROB:
-		return size_of_large_flows
+    # case: large flow
+    if random_float <= LARGE_FLOW_PROB:
+        return size_of_large_flows
 
-	# case: small flow
-	elif random_float <= SMALL_FLOW_PROB + LARGE_FLOW_PROB:
-		return size_of_small_flows
+    # case: small flow
+    elif random_float <= SMALL_FLOW_PROB + LARGE_FLOW_PROB:
+        return size_of_small_flows
 
-	# case: medium flow
-	else:
-		return size_of_medium_flows
+    # case: medium flow
+    else:
+        return size_of_medium_flows
 
 
 if __name__ == '__main__':
-	# little script to validate that the distribution of sizes
-	# matches the expected probabilities defined above
-	total = 10**6
-	sizes = [chen_distribution() for _ in range(total)]
-	counter = Counter(sizes)
-	for size, count in sorted(counter.items()):
-		fraction = count / total
-		print('{}% of flows have size {}'.format(int(100 * fraction), size))
+    # little script to validate that the distribution of sizes
+    # matches the expected probabilities defined above
+    total = 10**6
+    sizes = [chen_distribution() for _ in range(total)]
+    counter = Counter(sizes)
+    for size, count in sorted(counter.items()):
+        fraction = count / total
+        print('{}% of flows have size {}'.format(int(100 * fraction), size))
+
+
+chen_size = \
+        SMALL_FLOW_PROB  * 1 +
+        MEDIUM_FLOW_PROB * 10 +
+        LARGE_FLOW_PROB  * 1000
