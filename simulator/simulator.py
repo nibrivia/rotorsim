9ff62a79f8053b1903387f79d8dfe865b47362fb
Olivia Brode-Roger <nibr@mit.edu>
2019-12-25 01:16:39 -0500

    fixed a verbose type bug
    Former-commit-id: 86e6e39e00a8b5386ae05efb34a2a5a9bd59c1f6

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -185,7 +185,7 @@ def main(
                 print, "\033[1;91m@%.2f Cycle %s/%s\033[00m" % (
                     time, cycle+1, n_cycles),
                 priority = -100)
-        if not no_pause:
+        if verbose and not no_pause:
             R.call_in(time, print_demand, net.tors, priority=100)
             R.call_in(time, pause, priority=100)
 
