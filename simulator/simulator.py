c6a4ea15723bf4a4e7772ca3960ad3c23e79dbd3
Olivia Brode-Roger <nibr@mit.edu>
2020-01-24 22:37:36 -0500

    was printing something false...
    Former-commit-id: d056eb111e1b7c4bed50f0913cf111f3fe3b974a

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -171,7 +171,7 @@ def main(
     print("Time limit %dms, cycle %.3fms, slot %.3fms, slice %.3fms" %
             (time_limit, cycle_duration, slot_duration, slice_duration))
     print("#tor: %d, #rotor: %d, #links: %d, bw: %dGb/s, capacity: %.3fGb/s" %
-            (n_tor, net.n_rotor, n_tor*net.n_rotor, bandwidth/1e3, n_tor*net.n_rotor*bandwidth/1e3))
+            (n_tor, net.n_rotor, n_tor*net.n_rotor, bandwidth/1e3, n_tor*n_switches*bandwidth/1e3))
 
     print("Setting up flows, load %d%%..." % (100*load))
     # generate flows
