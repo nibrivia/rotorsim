a8b509772522f84e00be735e23348a1a72d1e2cc
Olivia Brode-Roger <nibr@mit.edu>
2020-04-16 12:52:44 -0400

    smaller cwnd for smaller memory usage?

--- simulator/flow.py
+++ simulator/flow.py
@@ -247,7 +247,7 @@ class TCPFlow(Flow):
     def _send_loop(self):
         if self.is_done:
             return
-        self.cwnd = 1000
+        self.cwnd = 100
 
         # Get next packet, send it
         while len(self.in_flight) + 1 <= self.cwnd:
